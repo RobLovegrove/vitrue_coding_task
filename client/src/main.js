c105caa77dd7a2d9fe3f8975029b012455e74c83
Rob Lovegrove <robertplovegrove@gmail.com>
2026-04-15 22:35:38 +0100

    refactor: Dividing components.css into seperate files per componenet

--- client/src/main.js
+++ client/src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
-import './styles.css'
-import './styles/components.css'
+import './styles/styles.css'
+import './styles/components/index.css'
 import App from './App.vue'
 
 createApp(App).mount('#app')
